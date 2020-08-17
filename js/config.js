7ae59ba4c0a25bdf229d33bc889daf7e852d451f
psaul20 <patricksaul20@gmail.com>
2020-08-16 20:11:36 -0500

    fixed config for real

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_IC5HMnP0N', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2uabs2jhmnanorpko5fj3l7usd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
